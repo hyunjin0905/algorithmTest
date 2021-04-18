8eb4789280be5d7d24d9df10b3b8c55117bd0059
조현진 <kkadangj@gmail.com>
2021-04-18 18:50:03 +0900

    [MOD] 삼각형 판별하기

--- js/section_01/test_03.js
+++ js/section_01/test_03.js
@@ -37,3 +37,20 @@ console.log(minData2);
 한 변의 길이와 양쪽 끝각의 크기를 알 때
 
  */
+
+function solution (a, b, c) {
+    let answer= "YES", max;
+    let sum = a +_b + c;
+    if (a > b) max = a;
+    else max = b;
+    if (c > max) max = c;
+
+    if ((sum - max) <= max) answer = "NO"
+
+
+
+    return answer;
+}
+
+console.log(solution(13,33,17))
+
