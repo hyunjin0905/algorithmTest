1a972daf7f9e136fe472e6fc44f6cace292a0225
조현진 <kkadangj@gmail.com>
2021-04-26 23:52:29 +0900

    [MOD] 홀수

--- js/section_01/test_04.js
+++ js/section_01/test_04.js
@@ -1,17 +1,18 @@
 // 홀수
 const searchOddNumber = (numList) => {
 
-    let oddNum = [];
+    //let oddNum = [];
     let sumOdd = 0;
     let minOdd;
     for (let i = 0; i < numList.length; i++){
+        let minOdd = numList[0]
         if (numList[i] % 2 !== 0) {
             sumOdd += numList[i];
-            oddNum.push(numList[i]);
+            if (minOdd < numList[i]) minOdd = numList[i]
         }
     }
 
-    minOdd = Math.min(...oddNum);
+  //  minOdd = Math.min(...oddNum);
     console.log(sumOdd, minOdd);
 
 }
@@ -21,6 +22,9 @@ const numbers = [12, 77, 38,41, 53, 92, 85];
 searchOddNumber(numbers);
 
 // 10 부제
+
+
+
 
 
 
